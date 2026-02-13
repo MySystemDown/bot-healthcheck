9b2258d06954632de6bcac454a1b96b379b4d386
Luiz Felipe Buaszczyk <luizfelipebuaszczyk@gmail.com>
2026-02-13 09:19:12 -0300

    chore: Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Bot - MySystemDown
+Este repositório tem o código fonte para o BOT que fica rodando em um System verificando endpoints previamente configurados.
---
+++ src/setttings.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    API_TOKEN = os.getenv("API_TOKEN")
+    API_URL = os.getenv("API_URL")
+    
+    BROKER_HOST=os.getenv("BROKER_HOST")
+    BROKER_PORT=os.getenv("BROKER_PORT")
+    BROKER_USER=os.getenv("BROKER_USER")
+    BROKER_PASSWORD=os.getenv("BROKER_PASSWORD")
